c4760a6e303e276b295a751fec440e5276539a08
Sebastien Binet <binet@cern.ch>
2014-06-24 18:31:07 +0200

    go-hsparse: no random

--- go/go-hsparse/main.go
+++ go/go-hsparse/main.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/go-hep/dao"
-	"github.com/go-hep/random"
 )
 
 var tests = []func(n int){
@@ -21,12 +20,12 @@ func main() {
 }
 
 func test_h1d(entries int) {
-	gauss := random.Gauss(1, 2, nil)
 	h1d := dao.NewH1D(100, -5, +5)
 	start := time.Now()
 	for i := 0; i < entries; i++ {
-		h1d.Fill(gauss(), 1.4)
+		x := float64(i%10) - 5
+		h1d.Fill(x, 1.4)
 	}
 	delta := time.Since(start)
-	fmt.Printf(" h1d.Fill(%d) = %v\n", entries, delta)
+	fmt.Printf(" h1d.Fill(%d) = %v (mean=%v rms=%v)\n", entries, delta, h1d.Mean(), h1d.RMS())
 }
